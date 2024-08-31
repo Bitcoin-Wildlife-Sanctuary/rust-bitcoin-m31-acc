2925c766cff53e16bb603cde428bb1aa392b29cb
weikengchen <w.k@berkeley.edu>
2024-09-01 01:03:07 +0800

    add mix_felt

--- src/dsl/sha256.rs
+++ src/dsl/sha256.rs
@@ -1,11 +1,11 @@
-use crate::dsl::qm31::reformat_qm31_to_dsl_element;
+use crate::dsl::qm31::{reformat_qm31_from_dsl_element, reformat_qm31_to_dsl_element};
 use crate::dsl::utils::draw_hints_to_memory_entries;
 use anyhow::Result;
 use bitcoin_circle_stark::channel::{ChannelWithHint, Sha256ChannelGadget};
 use bitcoin_circle_stark::treepp::*;
 use bitcoin_script_dsl::dsl::{Element, MemoryEntry, DSL};
 use bitcoin_script_dsl::functions::{FunctionMetadata, FunctionOutput};
-use stwo_prover::core::channel::Sha256Channel;
+use stwo_prover::core::channel::{Channel, Sha256Channel};
 use stwo_prover::core::vcs::sha256_hash::{Sha256Hash, Sha256Hasher};
 
 pub fn mix_digest(dsl: &mut DSL, inputs: &[usize]) -> Result<FunctionOutput> {
@@ -55,6 +55,30 @@ pub fn draw_felt_gadget(_: &[usize]) -> Result<Script> {
     })
 }
 
+pub fn mix_felt(dsl: &mut DSL, inputs: &[usize]) -> Result<FunctionOutput> {
+    let old_channel_digest = dsl.get_str(inputs[0])?.to_vec();
+    let felt = reformat_qm31_from_dsl_element(dsl.get_many_num(inputs[1])?);
+
+    let mut channel = Sha256Channel::default();
+    channel.update_digest(Sha256Hash::from(old_channel_digest));
+    channel.mix_felts(&[felt]);
+
+    Ok(FunctionOutput {
+        new_elements: vec![MemoryEntry::new(
+            "hash",
+            Element::Str(channel.digest().as_ref().to_vec()),
+        )],
+        new_hints: vec![],
+    })
+}
+
+pub fn mix_felt_gadget(_: &[usize]) -> Result<Script> {
+    Ok(script! {
+        4 OP_ROLL
+        { Sha256ChannelGadget::mix_felt() }
+    })
+}
+
 pub(crate) fn load_functions(dsl: &mut DSL) {
     dsl.add_function(
         "mix_digest",
@@ -73,13 +97,24 @@ pub(crate) fn load_functions(dsl: &mut DSL) {
             input: vec!["hash"],
             output: vec!["hash", "qm31"],
         },
+    );
+    dsl.add_function(
+        "mix_felt",
+        FunctionMetadata {
+            trace_generator: mix_felt,
+            script_generator: mix_felt_gadget,
+            input: vec!["hash", "qm31"],
+            output: vec!["hash"],
+        },
     )
 }
 
 #[cfg(test)]
 mod test {
+    use crate::dsl::qm31::reformat_qm31_to_dsl_element;
     use crate::dsl::{load_data_types, load_functions};
     use bitcoin_circle_stark::treepp::*;
+    use bitcoin_circle_stark::utils::get_rand_qm31;
     use bitcoin_script_dsl::dsl::{Element, DSL};
     use bitcoin_script_dsl::test_program;
     use rand::{Rng, SeedableRng};
@@ -164,5 +199,51 @@ mod test {
             },
         )
         .unwrap();
+    }
+
+    #[test]
+    fn test_mix_felt() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        let mut init_state = [0u8; 32];
+        init_state.iter_mut().for_each(|v| *v = prng.gen());
+        let init_state = Sha256Hash::from(init_state.to_vec());
+
+        let mut channel = Sha256Channel::default();
+        channel.update_digest(init_state);
+
+        let felt = get_rand_qm31(&mut prng);
+
+        channel.mix_felts(&[felt]);
+
+        let after = channel.digest().as_ref().to_vec();
+
+        let mut dsl = DSL::new();
+
+        load_data_types(&mut dsl);
+        load_functions(&mut dsl);
+
+        let old_channel_digest = dsl
+            .alloc_input("hash", Element::Str(init_state.as_ref().to_vec()))
+            .unwrap();
+        let felt_var = dsl
+            .alloc_input("qm31", Element::ManyNum(reformat_qm31_to_dsl_element(felt)))
+            .unwrap();
+
+        let res = dsl
+            .execute("mix_felt", &[old_channel_digest, felt_var])
+            .unwrap()[0];
+
+        assert_eq!(dsl.get_str(res).unwrap(), after);
+
+        dsl.set_program_output("hash", res).unwrap();
+
+        test_program(
+            dsl,
+            script! {
+                { after }
+            },
+        )
+        .unwrap()
     }
 }
