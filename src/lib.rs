9d26efbf48b4b8f85a2f82410ba0f008ad1da028
weikengchen <w.k@berkeley.edu>
2024-10-08 06:40:48 +0400

    rename worm to ldm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,2 @@
-pub fn report_bitcoin_script_size(category: &str, name: &str, script_size_bytes: usize) {
-    println!("{}.{} = {} bytes", category, name, script_size_bytes);
-}
-
 pub mod algorithms;
 pub mod dsl;
